1a60feaf6a1a37b3a9b213807bb0a6e63076d06b
Emanuele Gorga <emanuelegorga@gmail.com>
2022-01-30 17:18:17 +0000

    Add Bootstrap. Create Header and Footer components.

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Come Imparare</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/corsi">
+                <i className="fas fa-books"></i>Corsi
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
